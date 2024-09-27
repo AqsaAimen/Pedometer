38d675d6a529ff3eda516219090c79f1d20a1b6a
haier <aqsaaimen77@gmail.com>
2024-09-27 21:08:23 +0500

    final commit

---
+++ app/src/main/java/com/example/pedometerapp/Activity_main.kt
@@ -0,0 +1,11 @@
+package com.example.pedometerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
